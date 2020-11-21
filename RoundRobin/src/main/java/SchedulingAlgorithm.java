c205c2b018a867c9d1dbb93b7c555a2b2ca70897
jlareck <medik2507@gmail.com>
2020-11-22 00:27:06 +0200

    implemented round robin algorithm

--- RoundRobin/src/main/java/SchedulingAlgorithm.java
+++ RoundRobin/src/main/java/SchedulingAlgorithm.java
@@ -1,4 +1,4 @@
-// Run() is called from Scheduling.main() and is where
+// run() is called from Scheduling.main() and is where
 // the scheduling algorithm written by the user resides.
 // User modification should occur within the Run() function.
 
@@ -6,65 +6,80 @@ import java.util.Vector;
 import java.io.*;
 
 public class SchedulingAlgorithm {
+  static int comptime = 0;
+  static int currentProcess = 0;
+  static int nextProcess = 0;
+  static int totalNumberOfProcesses;
+  static int currentCompletedNumberOfProcesses = 0;
+  static int quantum = Scheduling.quantum;
+  static String resultsFile = "Summary-Processes";
+  static Process process;
+  static Vector processVector;
+  static PrintStream out;
 
-  public static Results Run(int runtime, Vector processVector, Results result) {
-    int i = 0;
-    int comptime = 0;
-    int currentProcess = 0;
-    int previousProcess = 0;
-    int size = processVector.size();
-    int completed = 0;
-    String resultsFile = "Summary-Processes";
-
-    result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "First-Come First-Served"; 
+  public static Results run(int runtime, Vector _processVector, Results result) {
+    processVector = _processVector;
+    totalNumberOfProcesses = processVector.size();
+    result.schedulingType = "Batch (Preemptive)";
+    result.schedulingName = "Round Robin Scheduling algorithm";
     try {
-      //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
-      //OutputStream out = new FileOutputStream(resultsFile);
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
-      sProcess process = (sProcess) processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+      out = new PrintStream(new FileOutputStream(resultsFile));
+      process = (Process) processVector.elementAt(currentProcess);
+      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")" + " Quantum " +quantum);
+
       while (comptime < runtime) {
         if (process.cpudone == process.cputime) {
-          completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-          if (completed == size) {
+          currentCompletedNumberOfProcesses++;
+          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+          if (currentCompletedNumberOfProcesses == totalNumberOfProcesses) {
             result.compuTime = comptime;
             out.close();
             return result;
           }
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-        }      
-        if (process.ioblocking == process.ionext) {
-          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-          process.numblocked++;
-          process.ionext = 0; 
-          previousProcess = currentProcess;
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime && previousProcess != i) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-        }        
-        process.cpudone++;       
-        if (process.ioblocking > 0) {
-          process.ionext++;
+          getNextProcess();
         }
-        comptime++;
+        if (quantum > 0) {
+          if (process.ioblocking == process.ionext) {
+            process.numblocked++;
+            out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+            process.ionext = 0;
+            getNextProcess();
+          }
+          if (process.ioblocking > 0) {
+            process.ionext++;
+          }
+          process.cpudone++;
+          quantum--;
+          comptime++;
+        } else {
+          out.println("Process: " + currentProcess + " stopped because of quantum... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " + quantum);
+          getNextProcess();
+        }
+
       }
       out.close();
     } catch (IOException e) { /* Handle exceptions */ }
     result.compuTime = comptime;
     return result;
+  }
+
+  public static void getNextProcess() {
+    int counter = 0;
+
+    nextProcess = currentProcess + 1;
+    if (nextProcess == processVector.size()) nextProcess = 0;
+    process = (Process) processVector.elementAt(nextProcess);
+
+    while (process.cpudone >= process.cputime) {
+      nextProcess++;
+      process = (Process) processVector.elementAt(nextProcess);
+    }
+
+    currentProcess = nextProcess;
+    process = (Process) processVector.elementAt(currentProcess);
+
+    out.println("\nProcess: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+
+    quantum = Scheduling.quantum;
   }
 }
