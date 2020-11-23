d6a2a130598b428ffd6689a9a94cb82fbe3830fc
jlareck <medik2507@gmail.com>
2020-11-23 15:54:03 +0200

    rewrote scheduling algorithm using queue

--- RoundRobin/src/main/java/Process.java
+++ RoundRobin/src/main/java/Process.java
@@ -5,11 +5,12 @@ public class Process {
   public int ionext;
   public int numblocked;
 
+
   public Process(int cputime, int ioblocking, int cpudone, int ionext, int numblocked) {
     this.cputime = cputime;
     this.ioblocking = ioblocking;
     this.cpudone = cpudone;
     this.ionext = ionext;
     this.numblocked = numblocked;
-  } 	
+  }
 }
--- RoundRobin/src/main/java/SchedulingAlgorithm.java
+++ RoundRobin/src/main/java/SchedulingAlgorithm.java
@@ -2,13 +2,14 @@
 // the scheduling algorithm written by the user resides.
 // User modification should occur within the Run() function.
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
 import java.io.*;
 
 public class SchedulingAlgorithm {
   static int comptime = 0;
   static int currentProcess = 0;
-  static int nextProcess = 0;
   static int totalNumberOfProcesses;
   static int currentCompletedNumberOfProcesses = 0;
   static int quantum = Scheduling.quantum;
@@ -20,30 +21,40 @@ public class SchedulingAlgorithm {
   public static Results run(int runtime, Vector _processVector, Results result) {
     processVector = _processVector;
     totalNumberOfProcesses = processVector.size();
+    Queue<Process> queue = new LinkedList<Process>(processVector);
     result.schedulingType = "Batch (Preemptive)";
     result.schedulingName = "Round Robin Scheduling algorithm";
     try {
       out = new PrintStream(new FileOutputStream(resultsFile));
-      process = (Process) processVector.elementAt(currentProcess);
+      process = queue.peek();
       out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")" + " Quantum " +quantum);
 
       while (comptime < runtime) {
         if (process.cpudone == process.cputime) {
+
           currentCompletedNumberOfProcesses++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+          out.println("Process: " + processVector.indexOf(process) + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
           if (currentCompletedNumberOfProcesses == totalNumberOfProcesses) {
             result.compuTime = comptime;
             out.close();
             return result;
           }
-          getNextProcess();
+          queue.poll();
+          process = queue.peek();
+          quantum = Scheduling.quantum;
+          out.println("\nProcess: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+
         }
         if (quantum > 0) {
           if (process.ioblocking == process.ionext) {
             process.numblocked++;
             out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
             process.ionext = 0;
-            getNextProcess();
+            queue.add(queue.poll());
+            process = queue.peek();
+            quantum = Scheduling.quantum;
+            out.println("\nProcess: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
+
           }
           if (process.ioblocking > 0) {
             process.ionext++;
@@ -53,36 +64,16 @@ public class SchedulingAlgorithm {
           comptime++;
         } else {
           out.println("Process: " + currentProcess + " stopped because of quantum... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " + quantum);
-          getNextProcess();
-        }
+          queue.add(queue.poll());
+          process = queue.peek();
+          quantum = Scheduling.quantum;
+          out.println("\nProcess: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
 
+        }
       }
       out.close();
     } catch (IOException e) { /* Handle exceptions */ }
     result.compuTime = comptime;
     return result;
-  }
-
-  public static void getNextProcess() {
-    nextProcess = currentProcess + 1;
-    if (nextProcess == processVector.size()) nextProcess = 0;
-    process = (Process) processVector.elementAt(nextProcess);
-
-    while (process.cpudone >= process.cputime) {
-
-      nextProcess++;
-      if (nextProcess == processVector.size()) {
-        nextProcess = 0;
-      }
-      process = (Process) processVector.elementAt(nextProcess);
-
-    }
-
-    currentProcess = nextProcess;
-    process = (Process) processVector.elementAt(currentProcess);
-    quantum = Scheduling.quantum;
-    out.println("\nProcess: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked + ")"  + " Quantum " +quantum);
-
-
   }
 }
